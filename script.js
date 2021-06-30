a9dc75c5d9053502e04c4b4c63f26bf3c92300b4
Nick De Raj <nickderaj@gmail.com>
2021-06-30 16:49:51 +0800

    Adding Leaflet, a public JS interactive map

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ if (navigator.geolocation) {
       const { latitude } = position.coords; // could write position.coords.latitude but this makes use of destructuring
       const { longitude } = position.coords;
       console.log(`https://www.google.com.my/maps/@${latitude},${longitude}`);
+
+      const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Could not get your position");
