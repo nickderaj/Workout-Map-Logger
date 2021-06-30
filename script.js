2526e8a3cd7f31ddf9a587c9c2318402b2cf000c
Nick De Raj <nickderaj@gmail.com>
2021-06-30 19:30:48 +0800

    Adding functionality to pushing workouts onto the page and fixing bug in code

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ class Running extends Workout {
     return this.pace;
   }
 }
+
 class Cycling extends Workout {
   type = "cycling";
 
@@ -46,8 +47,8 @@ class Cycling extends Workout {
   }
 
   calcSpeed() {
-    // min / km
-    this.pace = this.distance / (this.duration / 60);
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
     return this.speed;
   }
 }
@@ -162,6 +163,7 @@ class App {
     this._renderWorkoutMarker(workout);
 
     // Render workout on list
+    this._renderWorkout(workout);
 
     // Hide form + clear input fields
     inputDistance.value =
@@ -188,7 +190,7 @@ class App {
   }
 
   _renderWorkout(workout) {
-    const html = `
+    let html = `
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
       <h2 class="workout__title">${workout.description}}</h2>
       <div class="workout__details">
@@ -221,72 +223,21 @@ class App {
     if (workout.type === "cycling")
       html += `
     <div class="workout__details">
-        <span class="workout__icon">⏱</span>
-        <span class="workout__value">95</span>
-     <span class="workout__unit">min</span>
-    </div>
-    <div class="workout__details">
          <span class="workout__icon">⚡️</span>
          <span class="workout__value">${workout.speed.toFixed(1)}</span>
          <span class="workout__unit">km/h</span>
     </div>
      <div class="workout__details">
-          <span class="workout__icon">⛰</span>
-          <span class="workout__value">${workout.elevationGain}</span>
+          <span class="workout__icon">🚵‍♀️</span>
+          <span class="workout__value">${workout.elevation}</span>
          <span class="workout__unit">m</span>
     </div>
 </li>`;
+
+    form.insertAdjacentHTML("afterend", html);
   }
 }
 const app = new App();
-
-//  <li class="workout workout--running" data-id="1234567890">
-//   <h2 class="workout__title">Running on April 14</h2>
-//   <div class="workout__details">
-//     <span class="workout__icon">🏃‍♂️</span>
-//     <span class="workout__value">5.2</span>
-//     <span class="workout__unit">km</span>
-//   </div>
-//   <div class="workout__details">
-//     <span class="workout__icon">⏱</span>
-//     <span class="workout__value">24</span>
-//     <span class="workout__unit">min</span>
-//   </div>
-//           <div class="workout__details">
-//             <span class="workout__icon">⚡️</span>
-//             <span class="workout__value">4.6</span>
-//             <span class="workout__unit">min/km</span>
-//           </div>
-//           <div class="workout__details">
-//             <span class="workout__icon">🦶🏼</span>
-//             <span class="workout__value">178</span>
-//             <span class="workout__unit">spm</span>
-//           </div>
-//         </li>
-
-//         <li class="workout workout--cycling" data-id="1234567891">
-//           <h2 class="workout__title">Cycling on April 5</h2>
-//           <div class="workout__details">
-//             <span class="workout__icon">🚴‍♀️</span>
-//             <span class="workout__value">27</span>
-//             <span class="workout__unit">km</span>
-//           </div>
-//           <div class="workout__details">
-//             <span class="workout__icon">⏱</span>
-//             <span class="workout__value">95</span>
-//             <span class="workout__unit">min</span>
-//           </div>
-//           <div class="workout__details">
-//             <span class="workout__icon">⚡️</span>
-//             <span class="workout__value">16</span>
-//             <span class="workout__unit">km/h</span>
-//           </div>
-//           <div class="workout__details">
-//             <span class="workout__icon">⛰</span>
-//             <span class="workout__value">223</span>
-//             <span class="workout__unit">m</span>
-//           </div>
-//         </li>
 
 ////////////////////////////////////////////////////////////////
 
